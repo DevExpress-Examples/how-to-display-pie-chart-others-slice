31ab6673402556bafa5fabeaf4cb93d8c93cdbfd
DevExpressExampleBot <fakeEmail>
2020-01-20 19:17:12 +0300

    Source auto update [skip ci]

--- VB/PieChartOtherSliceExample/Form1.vb
+++ VB/PieChartOtherSliceExample/Form1.vb
@@ -5,39 +5,45 @@ Imports System.IO
 Imports System.Linq
 
 Namespace PieChartOtherSliceExample
-    Partial Public Class Form1
-        Inherits XtraForm
+	Partial Public Class Form1
+		Inherits XtraForm
 
-        Public Sub New()
-            InitializeComponent()
+		Public Sub New()
+			InitializeComponent()
 
-            AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf dashboardViewer1_ConfigureDataConnection
-            AddHandler dashboardViewer1.MasterFilterSet, AddressOf dashboardViewer1_MasterFilterSet
+			AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf dashboardViewer1_ConfigureDataConnection
+			AddHandler dashboardViewer1.MasterFilterSet, AddressOf dashboardViewer1_MasterFilterSet
+			AddHandler dashboardViewer1.MasterFilterCleared, AddressOf DashboardViewer1_MasterFilterCleared
 
-            dashboardViewer1.LoadDashboard("Dashboard.xml")
+			dashboardViewer1.LoadDashboard("Dashboard.xml")
+		End Sub
 
-        End Sub
 
-        Private Sub dashboardViewer1_MasterFilterSet(ByVal sender As Object, ByVal e As MasterFilterSetEventArgs)
-            Dim viewer As DashboardViewer = DirectCast(sender, DashboardViewer)
+		Private Sub dashboardViewer1_MasterFilterSet(ByVal sender As Object, ByVal e As MasterFilterSetEventArgs)
+			Dim viewer As DashboardViewer = DirectCast(sender, DashboardViewer)
 
-            If e.DashboardItemName = "gridDashboardItem1" Then
-                Dim stringValues = e.SelectedValues.Select(Function(value) value(1).ToString())
-                For Each selValue As String In stringValues
-                    viewer.Parameters("ParamSalesPerson").SelectedValues = stringValues
-                Next selValue
-            End If
-            If e.DashboardItemName = "rangeFilterDashboardItem1" Then
-                viewer.Parameters("ParamRangeStart").SelectedValue = e.SelectedRange.Minimum
-                viewer.Parameters("ParamRangeEnd").SelectedValue = e.SelectedRange.Maximum
-            End If
-        End Sub
+			If e.DashboardItemName = "gridDashboardItem1" Then
+				Dim stringValues = e.SelectedValues.Select(Function(value) value(1).ToString())
+				viewer.Parameters("ParamSalesPerson").SelectedValues = stringValues
+			End If
+			If e.DashboardItemName = "rangeFilterDashboardItem1" Then
+				viewer.Parameters("ParamRangeStart").SelectedValue = e.SelectedRange.Minimum
+				viewer.Parameters("ParamRangeEnd").SelectedValue = e.SelectedRange.Maximum
+			End If
+		End Sub
 
-        Private Sub dashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DashboardConfigureDataConnectionEventArgs)
-            Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
-            If parameters IsNot Nothing Then
-                parameters.FileName = Path.GetFileName(parameters.FileName)
-            End If
-        End Sub
-    End Class
+		Private Sub DashboardViewer1_MasterFilterCleared(ByVal sender As Object, ByVal e As MasterFilterClearedEventArgs)
+			Dim viewer As DashboardViewer = DirectCast(sender, DashboardViewer)
+			For Each p In viewer.Parameters.ToList()
+				p.SelectedValues = Nothing
+			Next p
+
+		End Sub
+		Private Sub dashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DashboardConfigureDataConnectionEventArgs)
+			Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
+			If parameters IsNot Nothing Then
+				parameters.FileName = Path.GetFileName(parameters.FileName)
+			End If
+		End Sub
+	End Class
 End Namespace
